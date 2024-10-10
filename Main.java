308f24f315a9c55319fba8be5d4321863096fb1b
Robert-Alexandru Chitu <62566438+aRobertChitu@users.noreply.github.com>
2024-10-10 22:43:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isPowerOfTwo(int n) {
+        if(n==0){
+            return false;
+        }
+        while(n != 1 && n%2 == 0){
+            n=n/2;
+        }
+        return n==1;
+    }
+}
